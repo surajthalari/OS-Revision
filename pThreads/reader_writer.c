24afa6afc9b9ca4533115de5a7d2a668c6c3e2d8
surajthalari@gmail.com <31706612+surajthalari@users.noreply.github.com>
2021-05-27 18:21:04 +0530

    complete reader_writer problem

--- pThreads/reader_writer.c
+++ pThreads/reader_writer.c
@@ -5,9 +5,9 @@
 #include <string.h>
 #include <limits.h>
 
-#define READER_COUNT 10
+#define READER_COUNT 5
+#define WRITER_COUNT 5
 
-FILE *fptr;
 char * file_path;
 int resource_counter = 0;
 
@@ -24,10 +24,18 @@ int count_lines();
 
 int main(int argc, char const *argv[])
 {
-	file_path = getcwd();
+	file_path = get_path();
 
-	pthread_t ThreadR[READER_COUNT], ThreadW;
+	pthread_t ThreadR[READER_COUNT], ThreadW[WRITER_COUNT];
 	int i=READER_COUNT;
+	int j = WRITER_COUNT;
+	while (j--)
+	{
+		if (pthread_create(&ThreadW[i], NULL, &writer, NULL))
+		{	
+			printf("Writer Thread failed to execute.\n");
+		}	
+	}
 
 	while(i--){
 		if (pthread_create(&ThreadR[i], NULL, &reader, NULL) != 0)
@@ -36,18 +44,109 @@ int main(int argc, char const *argv[])
 		}
 	}
 
-	if (pthread_create(&ThreadW, NULL, &writer, NULL))
+	j = WRITER_COUNT;
+	while (j--)
 	{
-		printf("Writer Thread failed to execute.\n");
+		pthread_join(ThreadW[j], NULL);
 	}
 
 	i = READER_COUNT;
 	while(i--){
 		pthread_join(ThreadR[i], NULL);
 	}
-	pthread_join(ThreadW, NULL);
 
 	return 0;
+}
+
+void *reader(void *args){
+	
+	pthread_mutex_lock(&mutex);
+	if (resource_counter == -1)
+	{
+		pthread_cond_wait(&read_phase, &mutex);
+	}
+	resource_counter++;
+	pthread_mutex_unlock(&mutex);
+
+	//Reading file
+	read_file();
+
+	pthread_mutex_lock(&mutex);
+	resource_counter--;
+	pthread_mutex_unlock(&mutex);
+	if (resource_counter == 0)
+	{
+		pthread_cond_broadcast(&write_phase);
+	}
+}
+
+void *writer(void *args){
+	pthread_mutex_lock(&mutex);
+	if (resource_counter != 0)
+	{
+		pthread_cond_wait(&write_phase, &mutex);
+	}
+	resource_counter = -1;
+	pthread_mutex_unlock(&mutex);
+
+	//Writing file
+	write_file();
+
+	pthread_mutex_lock(&mutex);
+	resource_counter = 0;
+	pthread_cond_broadcast(&read_phase);
+	pthread_cond_broadcast(&write_phase);
+	pthread_mutex_unlock(&mutex);
+}
+
+void read_file(){
+	printf("#####################read####################\n");
+	int count = count_lines();
+	printf("There are %d lines in the file\n", count);
+}
+
+void write_file(){
+	FILE *wptr = fopen(file_path, "a");
+	if (wptr == NULL)
+	{
+		perror("Unable to open the file");
+		exit(1);
+	}
+	printf("#################write#######################\n");
+	char c;
+	int count = 0;
+	for (c = getc(wptr); c != EOF; c = getc(wptr)){
+        if (c == '\n'){
+        	count = count + 1;
+        }     
+	}
+	char *data = (char*) malloc(7*sizeof(char));
+	strcpy(data, "Line : ");
+	char number = count + 1 + '0';
+	strncat(data, &number, 1);
+	fprintf(wptr, data);
+	fprintf(wptr, "\n");
+	printf("A line is written to the file\n");
+	printf("Now the total lines are %d\n", count + 1);
+	fclose(wptr);
+}
+
+int count_lines(){
+	FILE *fptr = fopen(file_path, "r");
+	if (fptr == NULL)
+	{
+		perror("Unable to open the file");
+		exit(1);
+	}
+	char c;
+	int count = 0;
+	for (c = getc(fptr); c != EOF; c = getc(fptr)){
+        if (c == '\n'){
+        	count = count + 1;
+        }     
+	}
+	fclose(fptr);
+	return count;
 }
 
 char* get_path(){
@@ -78,7 +177,7 @@ char* get_path(){
 	{
 		printf("File %s does not exist in the folder\n", file);
 		printf("%s\n", file_path);
-		fptr = fopen(file_path, "w+");
+		FILE *fptr = fopen(file_path, "w+");
 		if (fptr == NULL)
 		{
 			perror("unable to create the file");
@@ -92,22 +191,4 @@ char* get_path(){
 		printf("File %s exists in the folder\n", file);
 	}
 	return file_path;
-}
-
-int count_lines(){
-	fptr = fopen(file_path, "r");
-	if (fptr == NULL)
-	{
-		perror("Unable to open the file");
-		exit(1);
-	}
-	char c;
-	int count = 0;
-	for (c = getc(fptr); c != EOF; c = getc(fptr)){
-        if (c == '\n'){
-        	count = count + 1;
-        }     
-	}
-	fclose(fptr);
-	return count;
 }
